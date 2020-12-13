0fc95aab704eed3a6b8916e62646133b80aa7883
Nakatai <taiti5353@outlook.jp>
2020-12-13 16:57:45 +0900

    Add files via upload

---
+++ HelloWorld/src/com/wordpress/nakatai0322/helloworld/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.wordpress.nakatai0322.helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("");
+        System.out.println("See You!");
+    }
+}
