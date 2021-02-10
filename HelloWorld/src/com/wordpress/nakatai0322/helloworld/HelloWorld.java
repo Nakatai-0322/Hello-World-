89d524e8fa8a6e8d27d56e40e47708f74e379bcd
Nakatai <taiti5353@outlook.jp>
2021-02-11 08:08:54 +0900

    Delete HelloWorld directory

--- HelloWorld/src/com/wordpress/nakatai0322/helloworld/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package com.wordpress.nakatai0322.helloworld;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("");
-        System.out.println("See You!");
-    }
-}
